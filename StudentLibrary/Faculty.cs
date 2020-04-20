ec13ed522b480740f92eb049f7f3f57e97699822
Инна Тарасюк <1@1.ru>
2020-04-20 19:35:13 +0300

    First commit

---
+++ StudentLibrary/Faculty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLibrary
+{
+    public enum Faculty
+    {
+        CS,
+        MIEM,
+        Design
+    }
+}
